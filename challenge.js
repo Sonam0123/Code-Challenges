052adc4aa7e40870d6533f8af92307515715ebe9
sonam <tseringsonam34@hotmail.com>
2022-10-19 22:49:18 -0400

    subsequence pointers

--- challenge.js
+++ challenge.js
@@ -94,4 +94,18 @@ function areThereDuplicates() {
         return false
     }
 }
-console.log(areThereDuplicates(1, 2, 3, 4))
+// console.log(areThereDuplicates(1, 2, 3, 4))
+
+//subsequence string using pointers
+function isSubsequence(str1, str2) {
+    let i = 0;
+    let j = 0;
+    if (!str1) return true;
+    while (j < str2.length) {
+        if (str2[j] === str1[i]) i++;
+        if (i === str1.length) return true;
+        j++;
+    }
+    return false;
+}
+console.log(isSubsequence('abc', 'abraadcbra'))
